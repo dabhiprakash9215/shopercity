f322c3644646da8bf5d1ebe6dc2f852085d62a61
dabhi prakash <dabhiprakash32244@gmail.com>
2026-02-02 22:16:37 +0530

    add class

--- user/add-vendor.php
+++ user/add-vendor.php
@@ -217,16 +217,16 @@
 						<div class="col-12 row">
 							<div class="col-12 col-md-12 col-lg-6">
 								<label class="col-form-label">Facebook Link</label>
-								<input type="text" name="fb_link" value="<?php if (!empty($vendor_row['fb_link'])) {
-																				echo $vendor_row['fb_link'];
-																			} ?>">
+								<input type="text" class="form-control form-control-lg" name="fb_link" value="<?php if (!empty($vendor_row['fb_link'])) {
+																													echo $vendor_row['fb_link'];
+																												} ?>">
 							</div>
 							<div class="col-12 col-md-12 col-lg-6">
 
 								<label class="col-form-label">Instagram Link</label>
-								<input type="text" name="insta_link" value="<?php if (!empty($vendor_row['insta_link'])) {
-																				echo $vendor_row['insta_link'];
-																			} ?>">
+								<input type="text" class="form-control form-control-lg" name="insta_link" value="<?php if (!empty($vendor_row['insta_link'])) {
+																														echo $vendor_row['insta_link'];
+																													} ?>">
 							</div>
 						</div>
 						<div class="col-lg-6 col-12">
